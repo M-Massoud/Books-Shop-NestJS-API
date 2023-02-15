a0edb70aebcfa0b3a1738f4176c4bf124b36319a
M-Massoud <mohamed97masoud97@gmail.com>
2023-02-15 07:54:31 +0200

    update README file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
-    AuthModule,
     BooksModule,
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.DATABASE_ADMIN}:${process.env.DATABASE_PASSWORD}@cluster0.gzdimji.mongodb.net/?retryWrites=true&w=majority`
